b44ffaea0c604c50c4226f87e7bc4e42662e6fd4
Abilandou <godloveabilandou@gmail.com>
2024-10-01 15:01:24 +0100

    Implementation of Add To Cart Working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import React from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Collection from './pages/Collection';
@@ -15,9 +12,13 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes >
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -9,10 +10,53 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (productId, size) => {
+
+        if(!size) {
+            toast.error('Select Product Size');
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+        if(cartData[productId]){
+            if(cartData[productId][size]) {
+                cartData[productId][size] += 1;
+            }
+            else {
+                cartData[productId][size] = 1;
+            }
+        }
+        else{
+            cartData[productId] = {};
+            cartData[productId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const products in cartItems) {
+            for(const product in cartItems[products]){
+                try{
+
+                    if(cartItems[products][product] > 0){
+                        totalCount += cartItems[products][product];
+                    }
+
+                }catch(error){
+                    console.log(error);
+                }
+            }
+        }
+        return totalCount;
+    }
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart, getCartCount
     }
     return (
         <ShopContext.Provider value={value}>
